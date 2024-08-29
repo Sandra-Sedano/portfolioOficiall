f4fc342d992d2f949b60402b3b2a417c88a12eee
sandra-sedano <113152271+Sandra-Sedano@users.noreply.github.com>
2024-08-28 21:41:59 -0400

    atualizandoProjeto

---
+++ src/components/SobreMim.tsx
@@ -0,0 +1,10 @@
+export function SobreMim () {
+  return (
+  <>
+    <h2 className="text-6xl  text-center text-gray-800 mb-10 gap-4 flex-col mt-4">Sobre Mim</h2>
+    
+    
+    </>
+  )
+
+}
--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,6 +1,8 @@
 import { Avatar } from "../components/Avatar";
 import { CardsTrabalhos } from "../components/CardsTrabalhos";
+import { SobreMim } from "../components/SobreMim";
 import { ThemeController } from "../components/TheameContoller";
+
 
 
 
@@ -10,16 +12,24 @@ export function Home() {
         <>
             <section className="flex justify-center items-center  gap-4 flex-col mt-4 " >
                 <Avatar />
+
+             
+
                 <div className="text-2xl"> @sandra_siano </div>
                 <ThemeController />
                 <h1 className="text-4xl" >Analista de Sistemas</h1>
+
+                <div className="flex justify-center items-center  gap-4 flex-col mt-4 "> 
+                    
+                <SobreMim/>
+                    </div> 
             </section>
 
             <section className="flex justify-center items-center  gap-5 flex-col mt-4 " >
                 <div className="text-5xl gap-4 flex-col mt-4" color="#ffffff" >Um pouco dos projetos realizados </div>
                 
-                <CardsTrabalhos />
-               
+                <CardsTrabalhos  />
+              {/* /*  <CardsTrabalhos client:visible/> */}
             </section>
 
 
