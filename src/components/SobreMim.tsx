7a6f51ff76855e952c4114fe207b1734db6c1e0c
sandra-sedano <113152271+Sandra-Sedano@users.noreply.github.com>
2024-08-28 21:51:34 -0400

    SobreMin

--- src/components/SobreMim.tsx
+++ src/components/SobreMim.tsx
@@ -1,10 +1,15 @@
-export function SobreMim () {
-  return (
-  <>
-    <h2 className="text-6xl  text-center text-gray-800 mb-10 gap-4 flex-col mt-4">Sobre Mim</h2>
-    
-    
-    </>
-  )
+export function SobreMim() {
+    return (
+        <>
+            <h2 className="text-6xl  text-center text-gray-800 mb-10 gap-4 flex-col mt-4">Sobre Mim</h2>
+
+            <div className="text-6xl text-lg" > 
+                <h4>
+                    Olá, eu sou a Sandra e gosto do contato  com outros humanos 
+                </h4>
+
+            </div>
+        </>
+    )
 
 }
