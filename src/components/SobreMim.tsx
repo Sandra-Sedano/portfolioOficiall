ca1176473f0ee5310b5bc7d5139654ad4fc7ec7a
sandra-sedano <113152271+Sandra-Sedano@users.noreply.github.com>
2024-08-29 20:19:16 -0400

    Finalizando o sobre mim

--- src/components/SobreMim.tsx
+++ src/components/SobreMim.tsx
@@ -1,18 +1,23 @@
 export function SobreMim() {
     return (
         <>
-            <h2 className="text-3xl  text-center text-gray-800 mb-10 gap-4 flex-col mt-4">Sobre Mim</h2>
+            <h2 className="text-3xl text-center w-full mb-10 mt-4">
+                Sobre Mim
+            </h2>
 
-            <div className="text-6xl text-lg" >
-                <h4>
-                    Olá, eu sou a Sandra e gosto do contato  com outros humanos
-                </h4>
-                <h4>
-                    Trabalho com suporte e treinamento para cliente para Efisim Sistemas
-                </h4>
-
+            <div className="text-2xl w-full flex justify-center">
+                <div className="w-full max-w-3xl space-y-6">
+                    <p className="text-justify">
+                        Olá, me chamo Sandra, seja bem-vindo para conhecer um pouquinho sobre mim!
+                    </p>
+                    <p className="text-justify">
+                        Entrei no Tecnologia em Análise e Desenvolvimento de Sistemas - TADS em 2021, logo que saí do ensino médio, no auge da pandemia do COVID-19. Quando entramos, tivemos um semestre com EAD devido ao distanciamento social.
+                    </p>
+                    <p className="text-justify">
+                        Trabalho como suporte e treinamento para clientes na Efisim Sistemas, onde realizo treinamento e ensino a como usar o software ERP SisECF. Também atuo como suporte, tirando dúvidas que surgem no dia a dia. Nos tempos livres, realizo testes de novas versões que serão lançadas no sistema antes da implementação no cliente.
+                    </p>
+                </div>
             </div>
         </>
-    )
-
+    );
 }
